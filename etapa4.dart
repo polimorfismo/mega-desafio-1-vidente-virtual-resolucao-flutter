5d8e584ab1ffa5cb3b21162d2dabf14ea510cd68
lucasfv1 <51465884+lucasfv1@users.noreply.github.com>
2020-11-11 17:06:38 -0300

    Create etapa4.dart

---
+++ etapa4.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: VidenteVirtualPage(),
+  ));
+}
+
+class VidenteVirtualPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff332B25),
+      appBar: AppBar(
+        title: Text('Vidente Virtual'),
+        backgroundColor: Colors.brown[800],
+      ),
+      body: Vidente(),
+    );
+  }
+}
+
+class Vidente extends StatefulWidget {
+  @override
+  _VidenteState createState() => _VidenteState();
+}
+
+class _VidenteState extends State<Vidente> {
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: TextButton(
+        onPressed: () {
+          print('Botão pressionado');
+        },
+        child: Image.asset('imagens/vidente1.png'),
+      ),
+    );
+
+  }
+}
+
