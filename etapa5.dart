2f13c06f5bcf923277234c1184578240ab6c71c8
lucasfv1 <51465884+lucasfv1@users.noreply.github.com>
2020-11-11 18:19:27 -0300

    Create etapa5.dart

---
+++ etapa5.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: VidenteVirtualPage(),
+  ));
+}
+
+class VidenteVirtualPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff332B25),
+      appBar: AppBar(
+        title: Text('Vidente Virtual'),
+        backgroundColor: Colors.brown[800],
+      ),
+      body: Vidente(),
+    );
+  }
+}
+
+class Vidente extends StatefulWidget {
+  @override
+  _VidenteState createState() => _VidenteState();
+}
+
+class _VidenteState extends State<Vidente> {
+  int numeroDaImagem = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: TextButton(
+        onPressed: () {
+          numeroDaImagem = Random().nextInt(4);
+          print(numeroDaImagem);
+        },
+        child: Image.asset('imagens/vidente$numeroDaImagem.png'),
+      ),
+    );
+
+  }
+}
+
